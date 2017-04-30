06e6b688b20ade0c2227294b0cc1005cbe24ccda
Василий Суродин <gutskombayner@gmail.com>
2017-05-01 01:57:08 +0300

    delete some comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import React from 'react';
-//import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-//import agent from './agent';
-//import history from 'history';
 import store from './store';
 
 import './style/main.css'
